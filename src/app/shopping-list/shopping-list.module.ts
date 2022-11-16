0151f4bdbec8690978d849323b3da87e0d60e016
paul <paullin050182@gmail.com>
2022-11-17 00:30:31 +0800

    seperate appmodule to feature, core ,shared module

---
+++ src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,20 @@
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations:[ShoppingListComponent, ShoppingEditComponent],
+  imports:[
+    FormsModule,
+    RouterModule.forChild([
+      { path: "shopping-list", component: ShoppingListComponent },
+    ]),
+    SharedModule
+  ]
+})
+export class ShoppingListModule{
+
+}
