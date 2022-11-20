65b1e6a187b236c8fb969095a4a61f41bc3e285a
paul <paullin050182@gmail.com>
2022-11-20 14:42:10 +0800

    lazy loading practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,19 @@
 import { CoreModule } from './core.module';
 import { SharedModule } from "./shared/shared.module";
-import { ShoppingListModule } from "./shopping-list/shopping-list.module";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
-import { RecipesModule } from "./recipes/recipes.module";
-import { AuthModule } from './auth/auth.module';
+
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent,],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
-    RecipesModule,
-    ShoppingListModule,
-    AuthModule,
     AppRoutingModule,
     SharedModule,
     CoreModule
