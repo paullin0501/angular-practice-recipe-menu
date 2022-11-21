24cd93d73ab521ede8e1b44be9fb5bb3573a5bba
paul <paullin050182@gmail.com>
2022-11-21 22:57:13 +0800

    fix shopping list lazy loading error

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule, PreloadAllModules } from "@angular/router";
 const appRoutes: Routes = [
   { path: "", redirectTo: "/recipes", pathMatch: "full" },
   { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesModule'},
-  { path: 'shopping-list', loadChildren: './shopping-list#shopping-list.module#ShoppingListModule'},
+  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'},
   { path: 'auth', loadChildren: './auth/auth.module#AuthModule'},
 ];
 
