5337b5c2612a92fde374ff1821bf534571398b93
paul <paullin050182@gmail.com>
2022-11-22 23:04:13 +0800

    fix reducer problem

--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,21 +1,25 @@
-import { Action } from "@ngrx/store";
-import { Ingredient } from "../../shared/ingredient.model";
-import * as ShoppingListActions from "./shopping-list.actions";
+import { Ingredient } from '../../shared/ingredient.model';
+import * as ShoppingListActions from './shopping-list.actions';
 
-const initiaState = {
-  ingredients: [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10),
-  ]
+const initialState = {
+  ingredients: [new Ingredient('Apples', 5), new Ingredient('Tomatoes', 10)]
 };
 
-export function shoppingListReducer(state = initiaState,action: ShoppingListActions.AddIngredient){
-  switch (action.type){
+export function shoppingListReducer(
+  state = initialState,
+  action: ShoppingListActions.ShoppingListActions
+) {
+  switch (action.type) {
     case ShoppingListActions.ADD_INGREDIENT:
-    return {
-      ...state,
-      ingredient: [...state.ingredients, action.payload]
-    };
+      return {
+        ...state,
+        ingredients: [...state.ingredients, action.payload]
+      };
+    case ShoppingListActions.ADD_INGREDIENTS:
+      return {
+        ...state,
+        ingredients: [...state.ingredients, ...action.payload]
+      };
     default:
       return state;
   }
